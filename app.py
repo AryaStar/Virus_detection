103338e01172bb5da31004367e92011988b9287f
AryaStar <757451920@qq.com>
2021-10-07 19:37:56 +0800

    界面更新

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from Predict import predict
 import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/', methods = ['POST', 'GET'])
 def upload():
     if request.method == 'POST':
         f = request.files['file']
-        if f.filename =='':
+        if f.filename == '':
             return redirect(url_for('wrong'))
         basepath = os.path.dirname(__file__)
-        upload_path = os.path.join(basepath, 'report',secure_filename(f.filename))  # 注意：没有的文件夹一定要先创建，不然会提示没有该路径
+        upload_path = os.path.join(basepath, 'report', secure_filename(f.filename))  # 注意：没有的文件夹一定要先创建，不然会提示没有该路径
         f.save(upload_path)
         # upload_path = str(upload_path)
-        return redirect(url_for('result',path=f.filename))
+        return redirect(url_for('result', path=f.filename))
     return render_template('index.html')
 
 
@@ -29,7 +29,7 @@ def wrong():
 def result(path):
     result = predict('report/'+path)
     # return result
-    return render_template('result.html',result=result)
+    return render_template('result.html', result=result)
 
 
 if __name__ == '__main__':
